0326f24d26947bdc736d0a7522dc7a7079a6c85c
caijielu <2245675739@qq.com>
2021-11-24 09:34:49 +0800

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+// 用于命令行交互
+import { program } from "commander";
+
+import { helpOptions } from "./lib/core/help.js";
+
+import { createCommands } from "./lib/core/create.js";
+
+program.version("2.0.0");
+
+helpOptions();
+//创建指令
+createCommands();
+
+program.parse(process.argv);
